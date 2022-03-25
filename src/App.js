1fdefeb590e0d27176790c5c0b40dafc846fbcf0
Charles Callender <charlescallender.cc@gmail.com>
2022-03-25 16:59:58 -0700

    Created Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
+import Cart from './Cart';
 import Header from './Header';
+import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
+      <Routes>
+       
+          <Route exact path="/cart" element={<Cart/>}/>
+          <Route exact path="/" element={<Home/>}/>
+        
+    </Routes>
     </div>
+    </Router>
   );
 }
 
